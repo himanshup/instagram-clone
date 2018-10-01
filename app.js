2a419a0cd3a5885cf162c01c29a1e820c5ada2e3
himanshup <himanshup@csu.fullerton.edu>
2018-10-01 05:48:53 -0700

    require path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const helmet = require("helmet");
